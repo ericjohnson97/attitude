18de9fda3f67a005f6c41420e4bcc05f91f16c22
ericjohnson97 <eric.emj.johnson@gmail.com>
2018-12-03 10:45:49 -0600

    pretty much working

--- quatMult.m
+++ quatMult.m
@@ -5,10 +5,9 @@ q1v = q1(2:4);
 q2s = q2(1);
 q2v = q2(2:4);
 
-q3v = q2s*q1v + q1s*q2v - cross(q1v, q2v);
+q3v = q2s*q1v + q1s*q2v - cross(q2v, q1v);
 q3s = q1s*q2s - dot(q2v, q1v);
 
 q3 = [q3s; q3v];
-
 
 
